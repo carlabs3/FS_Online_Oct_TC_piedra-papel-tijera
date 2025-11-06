f8304e963ea031320d29bcab977ca8e6387ea996
leandrofuen <leandro_fuba@hotmail.es>
2025-11-06 12:55:03 +0000

    empezamos

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+const botones = document.querySelectorAll(".boton-jugada");
+const resultado = document.getElementById("resultados");
+let contM = document.getElementById("contador-ordenador");
+let contU = document.getElementById("contador-usuario");
+let puntosM = 0;
+let puntosU = 0;
+
+botones.forEach(boton => {
+    boton.addEventListener("click", function () {
+        let jugadaUsuario;
+        let resultadoMaquina;
+        console.log("Usuario ha sacado " + this.dataset.jugada);
+        jugadaUsuario = this.dataset.jugada;
+        resultadoMaquina = Math.floor(Math.random() * 3);
+        let jugadaMaquina;
+        
+        if (resultadoMaquina === 0) {
+
+            jugadaMaquina = "piedra"
+        } else if (resultadoMaquina === 1) {
+            jugadaMaquina = "papel"
+        } else {
+            jugadaMaquina = "tijera"
+        }
+        console.log("Maquina ha sacado " + jugadaMaquina)
+
+        if (jugadaMaquina === jugadaUsuario) {
+            console.log("empate");
+        }
+        else if ((jugadaMaquina === "piedra" && jugadaUsuario === "tijera") || (jugadaMaquina === "tijera" && jugadaUsuario === "papel") || (jugadaMaquina === "papel" && jugadaUsuario === "piedra")) {
+            console.log("Maquina ha ganado")
+            puntosM = puntosM + 1;
+        }
+        else {
+            console.log("Usuario ha ganado")
+            puntosU = puntosU + 1;
+
+        }
+        console.log(puntosM)
+        console.log(puntosU)
+
+    });
+});
+
+
+
+
